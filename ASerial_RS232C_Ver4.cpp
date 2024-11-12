3664d3e5dc666342ef34558dcb4c00af114c2b9b
O.Tappei <tappei0815@gmail.com>
2024-11-12 21:37:08 +0900

    コントラクタの作成

---
+++ ASerial_RS232C_Ver4.cpp
@@ -0,0 +1,13 @@
+#include "ASerial_RS232C_Ver4.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+
+ASerial::ASerial(uint32_t baudrate){
+    m_baudrate = baudrate;
+
+    
+
+}
